28476955cdb4bdfa20edfeab5cb762c7bb006800
misssn1998 <siennanguyen0503@gmail.com>
2024-03-19 14:45:56 +1300

    added TypeAnimation for hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,46 @@
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const handleScroll = () => {
-    const section = document.getElementById('contact')
-    section.scrollIntoView({smooth: true, duration: 500, block: "center"})
-  }
+    const section = document.getElementById("contact");
+    section.scrollIntoView({ smooth: true, duration: 500, block: "center" });
+  };
   return (
-    <div className="py-32" >
+    <div className="py-32">
       <div className="mx-auto max-w-7xl pl-32 pr-96">
         <article>
-          <h1 className="text-8xl font-bold tracking-wider">
+          <h1 className="text-7xl font-semibold tracking-wider">
             Hey, I'm Sienna
             <span>.</span>
           </h1>
-          <p className="mt-7 text-4xl tracking-wide">
+          <span className="mt-7 text-4xl tracking-wide">
             I'm a{" "}
-            <span className="text-primary font-black">
-              Full Stack Developer
-            </span>
-          </p>
+            <TypeAnimation
+              sequence={[
+                "Developer",
+                2000,
+                "Data Analyst",
+                2000,
+                "Microbiologist",
+                2000,
+              ]}
+              speed={50}
+              className="text-rose-500 font-semibold
+              "
+              wrapper="span"
+              repeat={Infinity}
+            />
+          </span>
           <p className="mt-3 text-lg tracking-wide">
             I'm switching from a career of microbiologist to a developer. I
-            enjoy coding and analyse data <br /> to create meaningful projects. Let's
-            connect!
+            enjoy coding and analyse data <br /> to create meaningful projects.
+            Let's connect!
           </p>
           <div>
-            <button className="btn btn-primary mt-6" onClick={handleScroll}>Contact Me</button>
+            <button className="btn btn-primary mt-6" onClick={handleScroll}>
+              Contact Me
+            </button>
           </div>
         </article>
       </div>
